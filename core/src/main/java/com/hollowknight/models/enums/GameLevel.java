79ff163a347c845d0802140e86816dd30bbd9c14
Reza <rezaghazavi2007@gmail.com>
2026-06-26 16:50:20 +0330

    spawn point

--- core/src/main/java/com/hollowknight/models/enums/GameLevel.java
+++ core/src/main/java/com/hollowknight/models/enums/GameLevel.java
@@ -1,5 +1,10 @@
 package com.hollowknight.models.enums;
 
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.PointMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
 
 public enum GameLevel {
@@ -12,6 +17,17 @@ public enum GameLevel {
     }
 
     public Vector2 getSpawnPoint() {
+        TmxMapLoader loader = new TmxMapLoader();
+        TiledMap map = loader.load(tmxPath);
+        MapLayer layer = map.getLayers().get("Solid");
+
+        for (MapObject object : layer.getObjects()) {
+            if ("Spawn Point".equals(object.getName())) {
+                PointMapObject pointMapObject = (PointMapObject) object;
+                return new Vector2(pointMapObject.getPoint().x, pointMapObject.getPoint().y);
+            }
+        }
+
         return new Vector2();
     }
 }
