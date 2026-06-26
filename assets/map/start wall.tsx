7bd3571e2fb10e3425f4e84db1753154d75eee1b
Reza <rezaghazavi2007@gmail.com>
2026-06-26 14:22:33 +0330

    added map in assets

---
+++ assets/map/start wall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="start wall" tilewidth="8" tileheight="8" tilecount="2236" columns="26">
+ <image source="../sprites/Architecture &amp; Environment/Area specfic architecture/Forgotten Crossroads/crossroads_false_door_0001_3.png" width="213" height="689"/>
+</tileset>
