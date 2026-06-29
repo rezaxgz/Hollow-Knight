9e593607c355e2a092fcb498d16290dcf3da2cf8
Reza <rezaghazavi2007@gmail.com>
2026-06-29 23:56:21 +0330

    extended map for crawler platform

---
+++ assets/map/FK_break__0000_wall_default.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="FK_break__0000_wall_default" tilewidth="8" tileheight="8" tilecount="1457" columns="31">
+ <image source="../sprites/Architecture &amp; Environment/Area specfic architecture/Forgotten Crossroads/FK_break__0000_wall_default.png" width="251" height="379"/>
+</tileset>
---
+++ assets/map/ruins_resting_extra_bits_0040_Layer-142.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="ruins_resting_extra_bits_0040_Layer-142" tilewidth="8" tileheight="8" tilecount="1122" columns="17">
+ <image source="../sprites/Architecture &amp; Environment/Signs/ruins_resting_extra_bits_0040_Layer-142.png" width="136" height="535"/>
+</tileset>
