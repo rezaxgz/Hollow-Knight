f69cd6295c0ce7d9bf64716dbc95e26ead55048d
Reza <rezaghazavi2007@gmail.com>
2026-06-30 15:08:49 +0330

    extended map

--- assets/map/ruins_resting_extra_bits_0040_Layer-142.tsx
+++ assets/map/ruins_resting_extra_bits_0040_Layer-142.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="ruins_resting_extra_bits_0040_Layer-142" tilewidth="8" tileheight="8" tilecount="1122" columns="17">
+<tileset version="1.10" tiledversion="1.12.2" name="ruins_resting_extra_bits_0040_Layer-142" tilewidth="16" tileheight="16" tilecount="1122" columns="8" tilerendersize="grid">
+ <grid orientation="orthogonal" width="14" height="14"/>
  <image source="../sprites/Architecture &amp; Environment/Signs/ruins_resting_extra_bits_0040_Layer-142.png" width="136" height="535"/>
 </tileset>
