3f31d1d2ee50a5df103004199d421f0aa0e51fab
Reza <rezaghazavi2007@gmail.com>
2026-06-30 16:55:28 +0330

    extended map for hornhead

---
+++ assets/map/pillar_cell.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="pillar_cell" tilewidth="8" tileheight="8" tilecount="294" columns="14">
+ <image source="../sprites/Architecture &amp; Environment/pillar_cell.png" width="118" height="173"/>
+</tileset>
---
+++ assets/map/plat_lifts_0002_a.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="plat_lifts_0002_a" tilewidth="8" tileheight="8" tilecount="392" columns="49">
+ <image source="../sprites/Architecture &amp; Environment/plat_lifts_0002_a.png" width="392" height="67"/>
+</tileset>
