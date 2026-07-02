aad2d1ce9876624f88aa2868cbca171c5c086167
Reza <rezaghazavi2007@gmail.com>
2026-07-03 01:16:51 +0330

    added mosscreep spawn

---
+++ assets/map/fung_new_0125_Layer-242.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="fung_new_0125_Layer-242" tilewidth="8" tileheight="8" tilecount="324" columns="18">
+ <image source="../sprites/Architecture &amp; Environment/Area specfic architecture/Greenpath/fung_new_0125_Layer-242.png" width="144" height="145"/>
+</tileset>
