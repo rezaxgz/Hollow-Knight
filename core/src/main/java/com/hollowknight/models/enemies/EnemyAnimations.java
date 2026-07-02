ed123205ffc74691a9771369ae406f78384f2db6
Reza <rezaghazavi2007@gmail.com>
2026-07-02 19:08:36 +0330

    changed some animations

--- core/src/main/java/com/hollowknight/models/enemies/EnemyAnimations.java
+++ core/src/main/java/com/hollowknight/models/enemies/EnemyAnimations.java
@@ -7,13 +7,13 @@ public enum EnemyAnimations {
     // CRAWLID
     CRAWLID_WALK("animation/Crawlid/Walk.png", 4, 1f, AnimationType.LOOP_PINGPONG),
     CRAWLID_TURN("animation/Crawlid/Turn.png", 2, Constants.CRAWLID_TURN_TIMER, AnimationType.ONESHOT),
-    CRAWLID_DEATH("animation/Crawlid/Death.png", 5, 2f, AnimationType.ONESHOT),
+    CRAWLID_DEATH("animation/Crawlid/Death.png", 5, 1f, AnimationType.ONESHOT),
 
     // Husk HornHead
     HORNHEAD_WALK("animation/Husk_Hornhead/Walk.png", 7, 1f, AnimationType.LOOP_PINGPONG),
     HORNHEAD_ATTACK_START("animation/Husk_Hornhead/Attack Anticipate.png", 5, 1f, AnimationType.ONESHOT),
     HORNHEAD_ATTACK_RUN("animation/Husk_Hornhead/Attack Lunge.png", 12, 1f, AnimationType.LOOP_PINGPONG),
-    HORNHEAD_DEATH("animation/Husk_Hornhead/Death Land.png", 8, 3f, AnimationType.ONESHOT),
+    HORNHEAD_DEATH("animation/Husk_Hornhead/Death Land.png", 8, 2f, AnimationType.ONESHOT),
     HORNHEAD_IDLE("animation/Husk_Hornhead/Idle.png", 6, 1f, AnimationType.LOOP),
     HORNHEAD_TURN("animation/Husk_Hornhead/Turn.png", 2, Constants.HORNHEAD_TURN_TIMER, AnimationType.ONESHOT),
     ;
