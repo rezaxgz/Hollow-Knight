9c87b5506f51630748dd32d4c6012225f775f15d
Reza <rezaghazavi2007@gmail.com>
2026-07-07 02:42:45 +0330

    connected bossroom to map

---
+++ assets/map/No_eyes_temple_0005_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="No_eyes_temple_0005_1" tilewidth="8" tileheight="8" tilecount="1175" columns="25">
+ <image source="../sprites/Architecture &amp; Environment/Area specfic architecture/Greenpath/No_eyes_temple_0005_1.png" width="203" height="382"/>
+</tileset>
